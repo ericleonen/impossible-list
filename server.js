55170e7aca1ace8f5565200cf33e15a45c4b7f85
ericleonen <ericleonen@gmail.com>
2022-12-18 22:22:40 -0800

    changed origin

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const goals = require("./routes/api/goals");
 const app = express();
 
 app.use(cors({
-    origin: "*"
+    origin: "https://impossible-list.onrender.com"
 }));
 
 // body-parser middleware
